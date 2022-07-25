00772134844d70d884b6024d11d9378e14d6cbfe
Rinat <eenot84@yandex.ru>
2022-07-25 23:19:06 +0300

    4

--- src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -1,10 +1,9 @@
-package ru.practicum.shareit.user;
+package ru.practicum.shareit.user.controller;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.mapper.UserMapper;
-import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.service.UserService;
 
 import javax.validation.Valid;
