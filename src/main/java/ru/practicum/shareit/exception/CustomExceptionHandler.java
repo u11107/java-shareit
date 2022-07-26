3d9a7882f6646aa152c521c8937f06b6042b3c91
Rinat <eenot84@yandex.ru>
2022-07-26 16:20:12 +0300

    9

--- src/main/java/ru/practicum/shareit/exception/CustomExceptionHandler.java
+++ src/main/java/ru/practicum/shareit/exception/CustomExceptionHandler.java
@@ -3,10 +3,14 @@ package ru.practicum.shareit.exception;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
