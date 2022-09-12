ec4f66429c76b9d8c04e10f4280d10d1c132c75c
Rinat <eenot84@yandex.ru>
2022-09-12 20:41:09 +0300

    Исправление замечаний

--- ShareIt-gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ ShareIt-gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -1,11 +1,15 @@
 package ru.practicum.shareit.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class ErrorHandler {
 
     @ExceptionHandler
@@ -13,7 +17,21 @@ public class ErrorHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ex.getMessage()));
     }
 
-    private class ErrorResponse {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(final Throwable ex) {
+        log.error(ex.getMessage());
+        return new ErrorResponse("Произошла непредвиденная ошибка");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handlerArgumentNotValidException(final MethodArgumentNotValidException ex) {
+        log.error(ex.getMessage());
+        return new ErrorResponse("Запрос недействителен");
+    }
+
+    private static class ErrorResponse {
         private String error;
 
         public ErrorResponse(String error) {
